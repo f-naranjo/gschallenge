1b415f9b6aaa9761124f5349a061f4ca71faa3b6
Fran Naranjo <fnaranjo88@gmail.com>
2020-03-04 13:03:42 +0100

    Update colors.js

--- client/src/styles/colors.js
+++ client/src/styles/colors.js
@@ -4,15 +4,8 @@ const appColors = {
     primary:"#272343",
     primarySoft:"#40425D",
     secondary:"#e3f6f5",
-    tertiary:"#bae8e8",
-    highlight:"#ffd803",
-    highlightSoft:"#FFF7AA",
     white:"#ffffff",
     gray:"#f6f6f6",
-    delete:"#FFCCD8",
-    create:"#D0EDA0",
-    edit:"#FFF7AA",
-    manage:"#A3CAD6",
 }
 
 export default appColors;
